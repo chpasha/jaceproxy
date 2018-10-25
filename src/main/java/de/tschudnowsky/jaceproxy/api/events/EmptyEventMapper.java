3c451685e364894753f2989dd9bcc0c4edb534b2
Pawel Tschudnowsky <pawel.tschudnowsky@tomcom.de>
2018-10-25 21:53:17 +0200

    do not instantiate empty events every time

--- src/main/java/de/tschudnowsky/jaceproxy/api/events/EmptyEventMapper.java
+++ src/main/java/de/tschudnowsky/jaceproxy/api/events/EmptyEventMapper.java
@@ -1,6 +1,5 @@
 package de.tschudnowsky.jaceproxy.api.events;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -8,23 +7,21 @@ import lombok.extern.slf4j.Slf4j;
  * Date: 07.10.18
  * Time: 13:36
  */
-@RequiredArgsConstructor
 @Slf4j
 public class EmptyEventMapper<T extends Event> implements EventMapper<T> {
 
-    private final Class<T> clazz;
     private T instance;
 
-    @Override
-    public T readValue(String rawValue) {
+    EmptyEventMapper(Class<T> clazz) {
         try {
-            if (instance == null) {
-                instance = clazz.newInstance();
-            }
-            return instance;
+            instance = clazz.newInstance();
         } catch (Exception e) {
             log.error("Instantiation of " + clazz, e);
         }
-        return null;
+    }
+
+    @Override
+    public T readValue(String rawValue) {
+        return instance;
     }
 }
