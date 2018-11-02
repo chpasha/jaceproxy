30bf52914ba289cc9fc514bc172f409bc268a18c
Pawel Tschudnowsky <pawel.tschudnowsky@tomcom.de>
2018-11-02 18:22:36 +0100

    do not log useless events

--- src/main/java/de/tschudnowsky/jaceproxy/acestream_api/EventLogger.java
+++ src/main/java/de/tschudnowsky/jaceproxy/acestream_api/EventLogger.java
@@ -1,31 +1,41 @@
 package de.tschudnowsky.jaceproxy.acestream_api;
 
-import de.tschudnowsky.jaceproxy.acestream_api.events.Event;
-import de.tschudnowsky.jaceproxy.acestream_api.events.StatusEvent;
+import de.tschudnowsky.jaceproxy.acestream_api.events.*;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.List;
+
+import static java.util.Arrays.asList;
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
 /**
  * User: pavel
  * Date: 07.10.18
  * Time: 14:09
- *
+ * <p>
  * Log interesting acestream events here
  */
 @ChannelHandler.Sharable
 @Slf4j
 public class EventLogger extends SimpleChannelInboundHandler<Event> {
 
+    private static final List<Class> discardEvents = asList(ResumeEvent.class, PauseEvent.class, LiveposEvent.class);
+
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, Event msg) {
-        if (msg instanceof StatusEvent) {
+        if (discardEvents.contains(msg.getClass())) {
+            log.trace("Discarding useless event {}", msg);
+        } else if (msg instanceof StatusEvent) {
             logState(((StatusEvent) msg));
+        } else {
+            if (msg instanceof HelloEvent) {
+                log.info("Connected to acestream engine version {}", ((HelloEvent) msg).getEngineVersion());
+            }
+            ctx.fireChannelRead(msg);
         }
-        ctx.fireChannelRead(msg);
     }
 
     private void logState(StatusEvent msg) {
